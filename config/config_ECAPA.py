99773a23aa8c2fc32d8d4ab8eb89076a153acae4
MirMag <142782660+MirMagchw@users.noreply.github.com>
2025-11-06 10:22:01 +0800

    Update config_ECAPA.py

--- config/config_ECAPA.py
+++ config/config_ECAPA.py
@@ -4,16 +4,16 @@ import torch as t
 from config.config import opt
 class DefaultConfig(object):
     def __init__(self):
-        self.env = 'default'  # visdom 环境
-        self.vis_port =8097 # visdom 端口
-        self.model_use = 'ECAPA_CNNModel'  # 使用的模型，名字必须与models/__init__.py中的名字一致
+        self.env = 'default' 
+        self.vis_port =8097 
+        self.model_use = 'ECAPA_CNNModel'
         self.ref_choice = "GCC_PATH"
         self.n_speaker = 2
         self.cluster_method = "FCM"
 
-        self.train_data_root = './data/train/'  # 训练集存放路径
-        self.test_data_root = './data/test1'  # 测试集存放路径
-        self.load_model_path = None  # 加载预训练的模型的路径，为None代表不加载
+        self.train_data_root = './data/train/' 
+        self.test_data_root = './data/test1' 
+        self.load_model_path = None
 
         self.batch_size = 32  # batch size
         self.use_gpu = True  # user GPU or not
@@ -26,14 +26,11 @@ class DefaultConfig(object):
         self.max_epoch = 20
         self.lr = 0.001  # initial learning rate
         self.lr_decay = 0.5  # when val_loss increase, lr = lr*lr_decay
-        self.weight_decay = 0e-5  # 损失函数
+        self.weight_decay = 0e-5  
         self.C = 512
         self.device = opt.device
 
     def _parse(self, kwargs):
-        """
-        根据字典kwargs 更新 config参数
-        """
         for k, v in kwargs.items():
             if not hasattr(self, k):
                 warnings.warn("Warning: opt has not attribut %s" % k)
